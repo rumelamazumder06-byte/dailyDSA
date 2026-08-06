654bb05573f53ebbdbffdb1838cf18b4cff03c73
Rumela Mazumder <rumelamazumder06@gmail.com>
2026-08-06 10:20:31 -0700

    Time: 39 ms (72.33%), Space: 45.5 MB (40.13%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,14 +1,38 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size(), count =0;
-        for(int i =0;i<n;i++){
-            int sum =0;
-            for(int j =i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k)
-                count ++;
-            }
+        // int n= nums.size();
+        // vector<int> ps(n,0);
+        // int count =0;
+        // unordered_map<int,int>m;
+        // ps[0]=nums[0];
+
+        // for(int i=1;i<n;i++){
+        //     ps[i]=ps[i-1]+nums[i];
+        // }
+        // for(int j=0;j<n;j++){
+        //     if(ps[j]==k) count++;
+        //    int val = ps[j]-k;
+        //     if(m.find(val)!=m.end())
+        //     count+=m[val];
+        //    if(m.find(val)==m.end()){
+        //     m[ps[j]]=0;}
+        //      m[ps[j]]++;
+        // }
+        // return count;
+
+
+        int n= nums.size();
+        unordered_map<int,int>m;
+        int curr=0; int count=0;
+        for(int i:nums){
+             curr+=i;
+             if(curr==k) count++;
+             int val =curr-k;
+             if(m.find(val)!=m.end())
+             count+=m[val];
+              
+                  m[curr]++;
         }
         return count;
     }
