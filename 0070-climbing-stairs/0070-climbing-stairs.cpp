47879b5ec900dd838c8904a92d826048a34efd39
Rumela Mazumder <rumelamazumder06@gmail.com>
2026-08-23 00:54:33 -0700

    Time: 0 ms (100%), Space: 7.7 MB (98.92%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+      if(n==1||n==2)
+      return n;
+
+      int a=1,b=2;
+      int temp=0;
+      for(int i=3;i<=n;i++){
+        temp=a+b;
+        a=b;
+        b=temp;
+      }
+      return temp;
+        
+    }
+};
